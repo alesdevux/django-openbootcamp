d81d1d1660bf5748c95512944c5acba810574594
alesdev <aleswebgit@gmail.com>
2023-10-12 00:53:48 +0200

    🗃️ Comments model & migration

--- modularitzacion/comentarios/models.py
+++ modularitzacion/comentarios/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Comments(models.Model):
+    name = models.CharField(max_length=50, null=False)
+    score = models.IntegerField(default=4)
+    text = models.TextField(max_length=1000, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return super().__str__()
